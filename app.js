f81b549f843d672d926e08dc3266ea5e57bbf67c
Barney Carroll <barney.carroll@gmail.com>
2019-04-24 23:56:32 +0100

    Bigger data set in text format
    Taken from ftp://ftp.unicode.org/Public/UNIDATA/UnicodeData.txt

--- app.js
+++ app.js
@@ -1,132 +1,88 @@
 import {m, css} from './deps.js'
 
-import Refresher from './Refresher.js'
 import Character from './Character.js'
 import Collapser from './Collapser.js'
 
-void (async () => {
-  const model = window.model = {
-    characters: [],
-    selection: new Set,
-    results: [],
-    loading: true,
-    groups: [],
-    query: '',
-  }
+const model = window.model = {
+  characters: [],
+  results: [],
+  loading: true,
+  groups: [],
+  query: '',
+}
 
-  await parse(await (await fetch('./ucd.nounihan.grouped.xml')).text())
+fetch('./UnicodeData.txt')
+  .then(response => response.text())
+  .then(text => {
+    model.characters = text.split('\n')
+      .map(entry => entry.split(';'))
+      .map(([cp, na1]) => ({cp, na1}))
 
-  model.loading = false
+    model.loading = false
+  })
 
-  m.mount(document.body, {
-    view: v => 
-      m('#App' + css`
-        margin: 1em;
-      `, [
-        m('h1', 'Charsearch'),
-          
-        m('p', 'A unicode character search application'),
+m.mount(document.body, {
+  view: v => 
+    m('#App' + css`
+      margin: 1em;
+    `, [
+      m('h1', 'Charsearch'),
+        
+      m('p', 'A unicode character search application'),
 
-        m(Collapser, ({toggle, indicator, contents}) => [
-          toggle(
-            m('h2', indicator(), 'Instructions'),
-          ),
-          
-          contents([
-            m('p', m('a[href=#source]', 'Configure'), ' the application by selecting the desired groups to search in, then start typing in the ', m('a[href=#search]', 'search input'), ' to filter results.'),
+      m(Collapser, ({toggle, indicator, contents}) => [
+        toggle(
+          m('h2', indicator(), 'Instructions'),
+        ),
+        
+        contents([
+          m('p', 'Charsearch uses provided search query to match against the descriptions of characters in the latest Unicode Character Database (excluding UniHan). By using tab navigation you can then select the desired character and hit ', m('kbd', 'Ctrl'), ' + ', m('kbd', 'C'), ' to copy it to the clipboard.'),
 
-            m('p', 'For example, if I select Latin_1_Sup, Latin_1_Sup, Latin_Ext_A, Latin_Ext_B groups, then I search for \'acute\', I should see the following characters: ĆćĹĺŃńŐőŔŕŚśŰűŹźǗǘ´ÁÉÍÓÚÝáéíóúý. By using tab navigation I can then select the desired character and hit ', m('kbd', 'Ctrl'), ' + ', m('kbd', 'C'), ' to copy it to the clipboard.'),
-
-            m('p', 'This application was developed by ', m('a[href=https://github.com/barneycarroll]', 'Barney Carroll'), '. The font is ', m('a[href=http://unifoundry.com/unifont/index.html]', 'GNU Unifont'), ', kindly converted to WOFF format by ', m('a[href=https://shkspr.mobi/blog/2019/04/banish-the-%EF%BF%BD-with-unifont/]', ' Terence Eden'), '.'),
-          ]),
-        ]),
-
-        m(Collapser, ({toggle, indicator, contents}) => [
-          toggle(
-            m('h2', indicator(), 'Search'),
-          ),
-
-          contents([
-            m('input#search', {
-              value: model.query,
-              oninput: ({target: {value}}) => {
-                model.query = value.toLowerCase()
-
-                search()
-              },
-            }),
-
-            m('hr'),
-
-              model.results.length === 0
-            ? 
-              model.query.length
-            ?
-              `No results matching "${ model.query }"`
-            :
-              `Start typing to see results`
-            : [
-              `Query "${ model.query }" yielded ${ model.results.length } results:`,
-
-              m('br'),
-
-              m('#Characters' + css`
-                margin: 1em;
-              `,
-                model.results.map(character => 
-                  m(Character, {character})
-                ),
-              ),
-            ]
-          ]),
-        ]),
-
-        m(Collapser, ({toggle, indicator, contents}) => [
-          toggle(
-            m('h2', indicator(), 'Config'),
-          ),
-
-          contents([
-            [model.loading && 'Loading...'],
-
-            [model.groups && model.groups.map((label, index) =>
-              m('label[style=display:block]',
-                m('input[type=checkbox]', {
-                  onclick: ({target: {checked}}) => {
-                    model.query = ''
-
-                    model.results = []
-
-                    model.selection[
-                      checked ? 'add' : 'delete'
-                    ](index)
-
-                    model.characters = Array.from(model.selection).reduce(
-                      (characters, index) => (
-                        characters.push(
-                          ...Array.from(
-                            model.xml.querySelectorAll('group')[index].children
-                          ).map(char => ({
-                            cp: char.getAttribute('cp'),
-                            na1: char.getAttribute('na1'),
-                          }))
-                        ),
-
-                        characters
-                      ),
-                      [],
-                    )
-                  },
-                }),
-
-                label,
-              )
-            )]
-          ]),
+          m('p', 'This application was developed by ', m('a[href=https://github.com/barneycarroll]', 'Barney Carroll'), '. The font is ', m('a[href=http://unifoundry.com/unifont/index.html]', 'GNU Unifont'), ', kindly converted to WOFF format by ', m('a[href=https://shkspr.mobi/blog/2019/04/banish-the-%EF%BF%BD-with-unifont/]', ' Terence Eden'), '.'),
         ]),
       ]),
-  })
-})()
+
+      m(Collapser, ({toggle, indicator, contents}) => [
+        toggle(
+          m('h2', indicator(), 'Search'),
+        ),
+
+        contents([
+          m('input#search', {
+            value: model.query,
+            oninput: ({target: {value}}) => {
+              model.query = value.toLowerCase()
+
+              search()
+            },
+          }),
+
+          m('hr'),
+
+            model.results.length === 0
+          ? 
+            model.query.length
+          ?
+            `No results matching "${ model.query }"`
+          :
+            `Start typing to see results`
+          : [
+            `Query "${ model.query }" yielded ${ model.results.length } results:`,
+
+            m('br'),
+
+            m('#Characters' + css`
+              margin: 1em;
+            `,
+              model.results.map(character => 
+                m(Character, {character})
+              ),
+            ),
+          ]
+        ]),
+      ]),
+    ]),
+})
 
 function search(){
   const {query} = model
@@ -145,32 +101,14 @@ function search(){
     )
 }
 
-function parse(xml){
-  model.loading = true
-
-  model.xml = (new DOMParser).parseFromString(
-    xml, 'application/xml',
+function parse(source){
+  const xml = (new DOMParser).parseFromString(
+    source, 'application/xml',
   )
 
-  if(model.xml.querySelector('parsererror'))
+  if(xml.querySelector('parsererror'))
     alert('Parser error: the XML appears to be malformed.')
   
-  model.groups = 
-    Array.from(
-      model.xml.querySelectorAll('group')
-    ).map(block => 
-      block.getAttribute('blk')
-    )
-  
-  model.loading = false
-}
-
-function ingest(file){
-  return new Promise((yes, no) => {
-    Object.assign(new FileReader(), {
-      onload : yes,
-      onerror : no,
-    })
-      .readAsText(file)
-  })
+  else
+    return xml
 }
